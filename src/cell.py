b1f21d136f6fd3910455b48df9ef00aed591cb5b
tontacchi <andy.tonthat14@gmail.com>
2025-04-27 16:31:07 -0700

    finished Cell and path rendering 1. Cell class 2. path is line drawn gray or red between cell centers

--- src/cell.py
+++ src/cell.py
@@ -1,47 +1,66 @@
+from __future__ import annotations
+
 from point import Point
 from line  import Line
 from window import Window
+
 
 class Cell:
     def __init__(self, upper_left_corner: Point, lower_right_corner: Point, window: Window) -> None:
         # [top, right, bottom, left]
         self.walls = [True] * 4
 
-        self._upper_left_corner  = upper_left_corner
-        self._lower_right_corner = lower_right_corner
+        self._upper_left  = upper_left_corner
+        self._lower_right = lower_right_corner
 
         self._window: Window = window
 
         return
 
-    def draw(self, upper_left: Point, lower_right: Point) -> None:
+    def draw(self) -> None:
         TOP, RIGHT, BOTTOM, LEFT = 0, 1, 2, 3
         draw_walls = []
 
         if self.walls[LEFT]:
-            x, y1, y2 = upper_left.x, upper_left.y, lower_right.y
+            x, y1, y2 = self._upper_left.x, self._upper_left.y, self._lower_right.y
             line = Line(Point(x, y1), Point(x, y2))
 
             draw_walls.append(line)
 
         if self.walls[TOP]:
-            y, x1, x2 = upper_left.y, upper_left.x, lower_right.x
-            line = Line(Point(y, x1), Point(y, x2))
+            y, x1, x2 = self._upper_left.y, self._upper_left.x, self._lower_right.x
+            line = Line(Point(x1, y), Point(x2, y))
 
             draw_walls.append(line)
         if self.walls[RIGHT]:
-            x, y1, y2 = lower_right.x, upper_left.y, lower_right.y
+            x, y1, y2 = self._lower_right.x, self._upper_left.y, self._lower_right.y
             line = Line(Point(x, y1), Point(x, y2))
 
             draw_walls.append(line)
         if self.walls[BOTTOM]:
-            y, x1, x2 = lower_right.y, upper_left.x, lower_right.x
-            line = Line(Point(y, x1), Point(y, x2))
+            y, x1, x2 = self._lower_right.y, self._upper_left.x, self._lower_right.x
+            line = Line(Point(x1, y), Point(x2, y))
 
             draw_walls.append(line)
 
         for line in draw_walls:
-            line.draw(self._window.__canvas, "black")
+            self._window.draw_line(line, "black")
+
+        return
+
+    def draw_move(self, dest_cell: Cell, undo: bool=False):
+        src_x = (self._upper_left.x + self._lower_right.x) // 2
+        src_y = (self._upper_left.y + self._lower_right.y) // 2
+
+        dest_x = (dest_cell._upper_left.x + dest_cell._lower_right.x) // 2
+        dest_y = (dest_cell._upper_left.y + dest_cell._lower_right.y) // 2
+
+        line = Line(Point(src_x, src_y), Point(dest_x, dest_y))       
+
+        if not undo:
+            self._window.draw_line(line, "red")
+        else:
+            self._window.draw_line(line, "gray")
 
         return
 
--- src/main.py
+++ src/main.py
@@ -12,16 +12,52 @@ def main() -> None:
 
     cell_list = []
 
-    cell_list.append(Cell(Point(5, 5), Point(10, 10), window))
-    cell_list.append(Cell(Point(10, 10), Point(15, 15), window))
-    cell_list.append(Cell(Point(15, 15), Point(20, 20), window))
+    temp1 = Cell(Point(100, 100), Point(150, 150), window)
+    temp1.walls = [False, False, False, False]
+    cell_list.append(temp1)
+
+    temp2 = Cell(Point(100, 50), Point(150, 100), window)
+    temp2.walls = [True, True, False, True]
+    cell_list.append(temp2)
+
+    temp3 = Cell(Point(50, 100), Point(100, 150), window)
+    temp3.walls = [True, False, True, True]
+    cell_list.append(temp3)
+
+    temp4 = Cell(Point(100, 150), Point(150, 200), window)
+    temp4.walls = [False, True, True, True]
+    cell_list.append(temp4)
+
+    temp5 = Cell(Point(150, 100), Point(200, 150), window)
+    temp5.walls = [True, True, True, False]
+    cell_list.append(temp5)
 
     for cell in cell_list:
         cell.draw()
 
+    temp1.draw_move(temp2)
+    temp1.draw_move(temp3)
+    temp1.draw_move(temp4)
+    temp1.draw_move(temp5)
+
     window.wait_for_close()
     return
 
+'''
+    side = 25
+    origin = Point(50, 50)
+    
+    NUM_ROWS, NUM_COLS = 3, 5
+    for row in range(NUM_ROWS):
+        start = Point(origin.x, origin.y)
+        start.y = origin.y + side * row
+
+        for col in range(NUM_COLS):
+            left  = Point(start.x, start.y)
+            right = Point(start.x + side, start.y + side)
+            cell_list.append(Cell(left, right, window))
+            start.x += side
+'''
 
 #---[ Entry ]-------------------------------------------------------------------
 if __name__ == "__main__":
